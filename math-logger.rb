1ac953a475a7ef3d364e7ec01a86eb4a8bda4942
Erick Davi Morgade Pessanha <erickdavi@gmail.com>
2017-08-02 17:53:45 -0300

    Update math-logger.rb

--- math-logger.rb
+++ math-logger.rb
@@ -5,12 +5,10 @@ class Registro
 			File.new(filename,"w+")
 		end	
 	end
-
 	def logar(op,num1,num2)
 		logfile = 'math.log'
 		self.criar(logfile)
 		log = Logger.new(logfile)
-
 		if (op == 'invalida' or (op == 'dividir' and num2 ==0)) 
 			log.error("#{op} #{num1} e #{num2} !!Operacao invalida")
 		else
