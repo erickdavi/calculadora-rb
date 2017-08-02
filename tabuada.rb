be6b1ecca7cb57129839fee33292081d4b29ad82
Erick Davi Morgade Pessanha <erickdavi@gmail.com>
2017-08-01 21:29:11 -0300

    Update tabuada.rb

--- tabuada.rb
+++ tabuada.rb
@@ -1,30 +1,43 @@
+#!/usr/bin/ruby
 require './calculadora-main.rb'
 
+repeticoes = 3
+operacoes = ["somar","subtrair","multiplicar","dividir"]
 aleatorio = Random.new
+calculadora = Calculadora.new
 
-valor1 = aleatorio.rand(10)
-valor2 = 0
-limite = valor2 + 2
-while(valor2 <= limite) do
-	resultado = somar(valor1,valor2)
-	puts "#{valor1} + #{valor2} = #{resultado}"
-	valor2 = valor2 + 1
-end
+operacoes.each do |op| 
+	case op
+	when "somar"
+		puts "Exemplos de Soma"	
+		repeticoes.times do
+			num1 = aleatorio.rand(10)
+			num2 = aleatorio.rand(10)
+			puts "#{num1} + #{num2} = #{calculadora.somar(num1,num2)}"
+		end
 
-while(valor2 <= limite) do
-        resultado = subtrair(valor1,valor2)
-        puts "#{valor1} - #{valor2} = #{resultado}"
-        valor2 = valor2 + 1
-end
+	when "subtrair"
+		puts "Exemplos de subtração"
+		repeticoes.times do
+			num1 = aleatorio.rand(10)
+			num2 = aleatorio.rand(10)
+			puts "#{num1} - #{num2} = #{calculadora.subtrair(num1,num2)}"			
+		end
 
-while(valor2 <= limite) do
-        resultado = multiplicar(valor1,valor2)
-        puts "#{valor1} X #{valor2} = #{resultado}"
-        valor2 = valor2 + 1
-end
+	when "multiplicar"
+		puts "Exemplos de multiplicação"
+		repeticoes.times do
+			num1 = aleatorio.rand(10)
+			num2 = aleatorio.rand(10)
+			puts "#{num1} X #{num2} = #{calculadora.multiplicar(num1,num2)}"			
+		end		
 
-while(valor2 <= limite) do
-        resultado = dividir(valor1,valor2)
-        puts "#{valor1} / #{valor2} = #{resultado}"
-        valor2 = valor2 + 1
+	when "dividir"
+		puts "Exemplos de divisão"
+		repeticoes.times do
+			num1 = aleatorio.rand(10).to_f
+			num2 = aleatorio.rand(10).to_f
+			puts "#{num1} / #{num2} = #{calculadora.dividir(num1,num2)}"			
+		end		
+	end
 end
